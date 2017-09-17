b368207c0536007038cc0416957e9033e9590610
Carlos André <carlos.ulrich@gmail.com>
2017-09-17 12:03:42 +0100

    Add Message model

---
+++ src/backend/chat/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+class Message(models.Model):
+    text = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+    def __unicode__(self):
+        return '[{timestamp}]: {text}'.format(**self.as_dict())
+
+    @property
+    def formatted_timestamp(self):
+        return self.timestamp.strftime('%b %-d %-I:%M %p')
+
+    def as_dict(self):
+        return {'text': self.text, 'timestamp': self.formatted_timestamp}
