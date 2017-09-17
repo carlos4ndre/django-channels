fbb9586bf3190381f59b18e399cdf467a00eeb04
Carlos André <carlos.ulrich@gmail.com>
2017-09-17 11:53:01 +0100

    Change messages endpoint to return data in JSON format

--- src/backend/chat/consumers.py
+++ src/backend/chat/consumers.py
@@ -1,18 +1,22 @@
-import json
-
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from channels import Group
 from channels.handler import AsgiHandler
 
 
+DUMMY_MESSAGES = [
+    {"text": "message #1"},
+    {"text": "message #2"},
+    {"text": "message #3"},
+]
+
 def http_index(message):
-    response = HttpResponse('HTTP index endpoint')
+    response = HttpResponse("HTTP index endpoint")
 
     for chunk in AsgiHandler.encode_response(response):
         message.reply_channel.send(chunk)
 
 def http_messages(message):
-    response = HttpResponse('HTTP messages endpoint')
+    response = JsonResponse({"messages": DUMMY_MESSAGES})
 
     for chunk in AsgiHandler.encode_response(response):
         message.reply_channel.send(chunk)
