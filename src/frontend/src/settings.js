08b75cadfd5368b871c245ef9ec708f4d8571254
Carlos André <carlos.ulrich@gmail.com>
2017-09-20 12:27:20 +0100

    Fix linting warnings

--- src/frontend/src/settings.js
+++ src/frontend/src/settings.js
@@ -2,7 +2,7 @@ const Settings = {
   NOTIFICATIONS_DELAY: 2000,
   WS_CHAT_URL: "ws://localhost:8000/chat",
   HTTP_MESSAGES_URL: "http://localhost:8000/messages",
-  HTTP_MAX_RETRIES: 3,
+  HTTP_MAX_RETRIES: 3
 }
 
 export default Settings;
